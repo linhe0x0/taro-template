56e239000f8f41ce0d6fbd1da0334df06ceb00a7
sqrtthree <imsqrtthree@gmail.com>
2020-10-17 11:48:49 +0800

    feat: Make it works

---
+++ config/index.js
@@ -0,0 +1,76 @@
+const path = require('path')
+const pkg = require('../package.json')
+
+const config = {
+  projectName: 'minapp',
+  date: '2020-10-17',
+  designWidth: 750,
+  deviceRatio: {
+    640: 2.34 / 2,
+    750: 1,
+    828: 1.81 / 2,
+  },
+  sourceRoot: 'src',
+  outputRoot: 'dist',
+  plugins: [],
+  defineConstants: {
+    APP_VERSION: `"${pkg.version}"`,
+  },
+  alias: {
+    '@': path.resolve(__dirname, '../src'),
+  },
+  sass: {
+    resource: path.resolve(__dirname, '..', 'src/styles/app.scss'),
+  },
+  copy: {
+    patterns: [],
+    options: {},
+  },
+  framework: 'react',
+  mini: {
+    postcss: {
+      pxtransform: {
+        enable: true,
+        config: {},
+      },
+      url: {
+        enable: true,
+        config: {
+          limit: 1024, // 设定转换尺寸上限
+        },
+      },
+      cssModules: {
+        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+        config: {
+          namingPattern: 'module', // 转换模式，取值为 global/module
+          generateScopedName: '[name]__[local]___[hash:base64:5]',
+        },
+      },
+    },
+  },
+  h5: {
+    publicPath: '/',
+    staticDirectory: 'static',
+    postcss: {
+      autoprefixer: {
+        enable: true,
+        config: {},
+      },
+      cssModules: {
+        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+        config: {
+          namingPattern: 'module', // 转换模式，取值为 global/module
+          generateScopedName: '[name]__[local]___[hash:base64:5]',
+        },
+      },
+    },
+  },
+}
+
+module.exports = function (merge) {
+  if (process.env.NODE_ENV === 'development') {
+    return merge({}, config, require('./dev'))
+  }
+
+  return merge({}, config, require('./prod'))
+}
---
+++ src/pages/index/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from '@tarojs/components'
+
+import './index.scss'
+
+export default function IndexPage() {
+  return (
+    <View className="Index">
+      <Text>Hello world!</Text>
+    </View>
+  )
+}
