c0ae27e16953343bf659e23c752bd6a58a197403
sqrtthree <imsqrtthree@gmail.com>
2020-10-17 12:04:17 +0800

    feat: Handle onUpdateReady event

--- src/app.js
+++ src/app.js
@@ -1,8 +1,30 @@
 import { Component } from 'react'
+import Taro from '@tarojs/taro'
 
 import './app.scss'
 
 class App extends Component {
+  componentDidShow() {
+    this.checkUpdate()
+  }
+
+  checkUpdate = () => {
+    const updateManager = Taro.getUpdateManager()
+
+    updateManager.onUpdateReady(() => {
+      Taro.showModal({
+        title: '更新提示',
+        content: '新版本已经备好啦，是否立即应用？',
+      })
+        .then((result) => {
+          if (result.confirm) {
+            updateManager.applyUpdate()
+          }
+        })
+        .catch(() => {})
+    })
+  }
+
   render() {
     return this.props.children
   }
